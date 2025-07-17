6db829fb05d4c3a902e597b7d226f945ffde4872
S-kaif123 <kaifbhombal.kb@gmail.com>
2025-07-17 12:24:58 +0530

    Starting with Light-Dark Converter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import "./App.css";
-<<<<<<< HEAD
-=======
 // import LoadMoreData from "./components/LoadMoreButton/LoadMoreData";
-import QrCodeGenerator from "./components/QrCode Generator/QrCodeGenerator"
->>>>>>> 7bcaafabfdf965c8c97cb344e45ce727f62c91de
+// import QrCodeGenerator from "./components/QrCode Generator/QrCodeGenerator"
 // import Accordian from "./components/Accordian/Accordian";
 // import ImageSlider from "./components/ImageSlider/ImageSlider";
 // import LoadMoreData from "./components/LoadMoreButton/LoadMoreData";
@@ -13,27 +10,6 @@ import QrCodeGenerator from "./components/QrCode Generator/QrCodeGenerator"
 // import data from "./components/TreeView/data.js";
 function App() {
   return (
-<<<<<<< HEAD
-    <>
-      {/* <Accordian />
-      <hr className="seperator" />
-      <RandomColor />
-      <hr className="seperator" />
-      <StarRating noOfStars={10} />
-      <hr className="seperator" />
-
-      <ImageSlider
-        url={"https://picsum.photos/v2/list"}
-        page={"1"}
-        limit={"10"}
-      />
-      <hr className="seperator" />
-    <LoadMoreData url={"https://dummyjson.com/products"}/>
-      <hr className="seperator" />
-      <TreeView  menu={data}/> 
-      */}
-   </>
-=======
     // <>
     //   <Accordian />
     //   <hr className="seperator" />
@@ -49,10 +25,9 @@ function App() {
     //   />
     //   <hr className="seperator" />
     // <LoadMoreData url={"https://dummyjson.com/products"}/>
+    // <QrCodeGenerator/>
     <>
-    <QrCodeGenerator/>
     </>
->>>>>>> 7bcaafabfdf965c8c97cb344e45ce727f62c91de
   );
 }
 
